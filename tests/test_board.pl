c50488dba69bab32dd90ac8e658e55e8e7c40c8f
rime3-14 <rim.chihab-eddine@insa-lyon.fr>
2025-12-15 23:02:03 +0100

    tests

--- tests/test_board.pl
+++ tests/test_board.pl
@@ -3,3 +3,28 @@
 :- use_module(library(plunit)).
 % Import des modules du projet
 :- use_module('../src/board').
+
+:- begin_tests(board).
+
+test(init_creates_board) :-
+	retractall(board(_)),
+	length(C0,6), length(C1,6), length(C2,6), length(C3,6), length(C4,6), length(C5,6), length(C6,6),
+	Board = [C0,C1,C2,C3,C4,C5,C6],
+	assert(board(Board)),
+	board(B), length(B,7),
+	forall(member(Col, B), length(Col,6)),
+	retractall(board(_)).
+
+test(applyIt_updates_board) :-
+	% original empty board
+	length(A0,6), length(A1,6), length(A2,6), length(A3,6), length(A4,6), length(A5,6), length(A6,6),
+	Board = [A0,A1,A2,A3,A4,A5,A6],
+	retractall(board(_)), assert(board(Board)),
+	% create a new board with an 'x' at column 2, row 1
+	B0 = A0, B1 = A1, B2 = [_,x,_,_,_,_], length(B3,6), length(B4,6), length(B5,6), length(B6,6),
+	NewBoard = [B0,B1,B2,B3,B4,B5,B6],
+	applyIt(Board, NewBoard),
+	board(B), nth0(2,B,Col2), nth0(1,Col2,Val), assertion(Val == x),
+	retractall(board(_)).
+
+:- end_tests(board).
--- tests/test_minimax.pl
+++ tests/test_minimax.pl
@@ -1,21 +1,22 @@
-:- module(test_game, []).
+:- module(test_minimax, []).
 
 :- use_module(library(plunit)).
 
 % Import des modules du projet
 :- use_module('../src/winner').
 :- use_module('../src/minimax').
+:- use_module('../src/ai').
 
 :- begin_tests(minimax).
 
-/*verifications de la fonction utility*/
-test(utility_returns_1_for_x_win) :-
+/* verifications de la fonction utility (signature utility(Board,Player,Utility)) */
+test(utility_positive_for_x_win) :-
     Col0 = ['x','x','x','x',_,_],
     length(C1,6), length(C2,6), length(C3,6), length(C4,6), length(C5,6), length(C6,6),
     Board = [Col0,C1,C2,C3,C4,C5,C6],
-    utility(Board, U), assertion(U == 1).
+    utility(Board, x, U), assertion(U >= 10000).
 
-test(utility_returns_minus1_for_o_win) :-
+test(utility_positive_for_o_win_when_evaluating_o) :-
     % horizontal o win on row 1 across columns 1..4
     C0 = [_,_,_,_,_,_],
     C1 = [_,o,_,_,_,_],
@@ -24,15 +25,15 @@ test(utility_returns_minus1_for_o_win) :-
     C4 = [_,o,_,_,_,_],
     length(C5,6), length(C6,6),
     Board = [C0,C1,C2,C3,C4,C5,C6],
-    utility(Board, U), assertion(U == -1).
+    utility(Board, o, U), assertion(U >= 10000).
 
 test(utility_returns_0_for_empty_board_and_isEmpty_true) :-
     length(C0,6), length(C1,6), length(C2,6), length(C3,6), length(C4,6), length(C5,6), length(C6,6),
     Board = [C0,C1,C2,C3,C4,C5,C6],
     isBoardEmpty(Board),
-    utility(Board, U), assertion(U == 0).
+    utility(Board, x, U), assertion(U == 0).
 
-/*verifications de firstFreeIndexColonne : doit retourner l'index libre dans une colonne*/
+/* verifications de firstFreeIndexColonne : doit retourner l'index libre dans une colonne */
 test(firstFreeIndexColonne_returns_index) :-
     % Column 0 has two filled then free at index 2
     C0 = [x,o,_,_,_,_],
@@ -46,7 +47,7 @@ test(firstFreeIndexColonne_returns_6_for_full_column) :-
     Board = [Cfull,C1,C2,C3,C4,C5,C6],
     firstFreeIndexColonne(Board, 0, Index), assertion(Index == 6).
 
-/*teste les moves possibles*/
+/* teste les moves possibles */
 test(possible_moves_excludes_full_columns) :-
     Cfull = [x,o,x,o,x,o],
     Cfree = [_,_,_,_,_,_],
